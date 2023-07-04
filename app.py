5a04132b95a361e8ff6233d037e075a23989ee69
Evilmoose <ronnielopez@gmail.com>
2023-07-03 23:55:54 -0700

    Add a route

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 from flask import Flask, render_template, request, flash
 import requests
 import json
+
+app = Flask(__name__)
+app.secret_key = 'mysecretkey'
+
+@app.route('/', )
+def index():
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
