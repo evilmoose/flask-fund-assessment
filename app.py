0058552f5d53d71ba7365e19f86c496d6ce49eca
Evilmoose <ronnielopez@gmail.com>
2023-07-04 00:11:03 -0700

    Check if currencies and amount are valid

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, flash
 import requests
 import json
+from utils import is_valid_currency
 
 app = Flask(__name__)
 app.secret_key = 'mysecretkey'
@@ -12,10 +13,17 @@ def index():
         to_currency = request.form['to']
         amount = request.form['amount']
 
+        if not is_valid_currency(from_currency) or not is_valid_currency(to_currency):
+            flash('Please input valid currency codes', 'error')
+            return render_template('index.html')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            flash('Please input a valid number for amount', 'error')
+            return render_template('index.html')
         
-
-
-    return render_template('index.html')
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
