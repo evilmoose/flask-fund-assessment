9bca9e64e82524493261d7d783a7372258f1f6ef
Evilmoose <ronnielopez@gmail.com>
2023-07-04 00:42:30 -0700

    Bug fixes

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.secret_key = 'mysecretkey'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-     if request.method == 'POST':
+    if request.method == 'POST':
         from_currency = request.form['from']
         to_currency = request.form['to']
         amount = request.form['amount']
@@ -34,7 +34,8 @@ def index():
             else:
                 flash('Error in currency conversion. Please try again', 'error')
 
-        return render_template('index.html')
+        
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
